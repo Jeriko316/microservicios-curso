ef643f0a41aaef42c83953f4e7658d076a38c63e
Jeriko316 <122418627+Jeriko316@users.noreply.github.com>
2023-01-11 10:56:59 -0500

    Rest template - feing client

---
+++ usuario-service/src/main/java/com/usuario/service/entidades/Usuario.java
@@ -0,0 +1,43 @@
+package com.usuario.service.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //indicamos q va a ser auto aumentable
+	private int id;
+	private String nombre;
+	private String mail;
+	
+	
+	
+	
+	
+	//getters y setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+
+}
